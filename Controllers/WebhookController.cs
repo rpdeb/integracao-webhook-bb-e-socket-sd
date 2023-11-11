0453cde5ac83c33fdc5612ee1320f3c039b3a77a
rpdeb <deboraribeiro9015@gmail.com>
2023-11-11 04:05:17 -0300

    Commit Inicial
    Commit Inicial

---
+++ Controllers/WebhookController.cs
@@ -0,0 +1,26 @@
+﻿using ApiPixBB.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPixBB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebhookController : ControllerBase
+    {
+        
+        [HttpGet("recebimento")]
+        public string Get(
+            ReceberPix receberPix)
+        {
+            return "Pix Recebido";
+        }
+
+        [HttpPost("envio")]
+        public string Post(
+           ReceberPix receberPix)
+        {
+            return "Pix Enviado";
+        }
+    }
+}
---
+++ Model/ReceberPix.cs
@@ -0,0 +1,38 @@
+﻿namespace ApiPixBB.Model
+{
+    public class ReceberPix
+    {
+        public class ComponentesValor
+        {
+            public Original original { get; set; }
+            public string chave { get; set; }
+            public string horario { get; set; }
+            public string infoPagador { get; set; }
+            public Pagador pagador { get; set; }
+        }
+
+        public class Original
+        {
+            public string valor { get; set; }
+        }
+
+        public class Pagador
+        {
+            public string cpf { get; set; }
+            public string nome { get; set; }
+        }
+
+        public class Pix
+        {
+            public string endToEndId { get; set; }
+            public string txtid { get; set; }
+            public string valor { get; set; }
+            public ComponentesValor componentesValor { get; set; }
+        }
+
+        public class Root
+        {
+            public List<Pix> pix { get; set; }
+        }
+    }
+}
